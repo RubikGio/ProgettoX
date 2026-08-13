6d5578e570ac78164b31b8251145da0432031a64
Giovanni <giovanni.ferraro10@studenti.unina.it>
2026-08-13 17:53:30 +0200

    Add files via upload

---
+++ Core/Inc/display.h
@@ -0,0 +1,13 @@
+#ifndef INC_DISPLAY_H_
+#define INC_DISPLAY_H_
+
+#include <stdint.h>
+#include "SSD1306.h"
+#include "globalVariable.h"
+
+void layoutDisplay_pad(uint8_t *buffer);
+void layoutDisplay_drone(uint8_t *buffer);
+void updateValue(uint8_t *buffer, uint8_t riga, uint8_t dato);
+void updateAll(DualsenseData dsd, uint8_t *stato, uint8_t *buffer);
+
+#endif /* INC_DISPLAY_H_ */
